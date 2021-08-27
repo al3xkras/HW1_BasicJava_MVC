9559e513601b60d8396641f89e2e5aa52889f3c9
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2021-08-27 10:35:22 +0300

    View: access modifiers updated

--- src/main/java/org/example/View.java
+++ src/main/java/org/example/View.java
@@ -1,28 +1,28 @@
 package org.example;
 
 public class View {
-    final String MESSAGE_ON_START =
+    public static final String MESSAGE_ON_START =
             "Input words to create phrase \"Hello world!\"\nInput word:";
-    final String MESSAGE_WRONG_INPUT =
+    public static final String MESSAGE_WRONG_INPUT =
             "Wrong input! Try again.";
-    final String MESSAGE_INPUT_NEXT_WORD =
+    public static final String MESSAGE_INPUT_NEXT_WORD =
             "Great! Now input next word:";
-    final String MESSAGE_PHRASE_COMPLETED =
+    public static final String MESSAGE_PHRASE_COMPLETED =
             "Phrase completed:";
 
-    public void messageOnStart(){
+    public static void messageOnStart(){
         System.out.println(MESSAGE_ON_START);
     }
 
-    public void messageWrongInput(){
+    public static void messageWrongInput(){
         System.out.println(MESSAGE_WRONG_INPUT);
     }
 
-    public void messageInputNextWord(){
+    public static void messageInputNextWord(){
         System.out.println(MESSAGE_INPUT_NEXT_WORD);
     }
 
-    public void messagePhraseCompleted(){
+    public static void messagePhraseCompleted(){
         System.out.println(MESSAGE_PHRASE_COMPLETED);
     }
 
